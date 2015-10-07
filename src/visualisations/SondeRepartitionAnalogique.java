dd39d8612ed4f3c60b5534cd13946bd8580377ec
Antoine GIRARD <sapk@sapk.fr>
2015-10-07 13:29:16 +0200

    Test signal carré en commentaire

--- src/visualisations/SondeRepartitionAnalogique.java
+++ src/visualisations/SondeRepartitionAnalogique.java
@@ -9,7 +9,9 @@ import information.Information;
  * @author Antoine GIRARD
  */
 public class SondeRepartitionAnalogique extends Sonde<Float> {
+
     //TODO use a cubic value
+
     private float resolution = 1f;
     private final float max;
     private final float min;
@@ -25,30 +27,39 @@ public class SondeRepartitionAnalogique extends Sonde<Float> {
         this.min = min;
         this.max = max;
     }
+
     public SondeRepartitionAnalogique(String nom, float min, float max, float resolution) {
-        this(nom,min,max);
+        this(nom, min, max);
         this.resolution = resolution;
     }
+
     public SondeRepartitionAnalogique(String nom, float min, float max, float resolution, int nbPixels) {
-        this(nom,min,max,resolution);
+        this(nom, min, max, resolution);
         this.nbPixels = nbPixels;
     }
 
     public void recevoir(Information<Float> information) {
         informationRecue = information;
         int nbElements = (int) ((max - min) / resolution);
-        System.out.println("max : "+max+" min : "+min+" nbElements : "+nbElements);
+        System.out.println("max : " + max + " min : " + min + " nbElements : " + nbElements);
+        //float[] table = new float[nbElements*2]; //on multiplie par 2 afin de faire une signal carré
         float[] table = new float[nbElements];
         for (float f : information) {
             //System.out.println("Test : " + f + /*" / " + f % resolution + */" / "+(f-(f%resolution))+" / "+(int)(f-(f%resolution) - min)*1/resolution + " / " +(int)(f*resolution) - min );
             //table[(int) ((f - (f % resolution) - min) * 1 / resolution)]++;
-            if(f>min && f<max){
-                table[(int)((f-min)*1/resolution)]++;
-            }else{
-                System.out.println("Value hors intervalle : "+f+" Index : "+(int)((f-min)*1/resolution));
+            if (f > min && f < max) {
+                /*
+                 int index = (int)((f-min)*1/resolution)*2;
+                 table[index]++;
+                 table[index+1]++;
+                 */
+                 int index = (int)((f-min)*1/resolution);
+                 table[index]++;
+            } else {
+                System.out.println("Value hors intervalle : " + f + " Index : " + (int) ((f - min) * 1 / resolution));
             }
         }
-        new VueCourbe(table,nbPixels, nom);
+        new VueCourbe(table, nbPixels, nom);
     }
 
 }
