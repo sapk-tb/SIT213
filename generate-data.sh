78b53bd089a9aacebfe3ae74eebe05884a81767f
Antoine GIRARD <sapk@sapk.fr>
2015-10-08 15:35:47 +0200

    Mise en place des paramètres finaux

--- generate-data.sh
+++ generate-data.sh
@@ -3,16 +3,19 @@
 
 function get-teb-by-snr {
     NB_ECH=30
-    NB_SYM=10000
-    ARGS="-form NRZ -mess $NB_SYM -nbEch $NB_ECH -ampl -1 1"
+    NB_SYM=999999
+    FORM=${2:-"NRZ"}
+    ARGS="-form $FORM -mess $NB_SYM -nbEch $NB_ECH -ampl -1 1"
     ./simulateur $ARGS -snr $1 | grep TEB | cut -d":" -f2 | tr -d " "
 }
 function generate-teb-by-snr {
-    echo "SNR,TEB"
+    echo "SNR,TEB_RZ,TEB_NRZ,TEB_NRZT"
     for snr in $(seq $1 1 $2)
     do
-	TEB=$(get-teb-by-snr $snr)
-	echo "$snr,$TEB"
+        TEB_RZ=$(get-teb-by-snr $snr "RZ")
+        TEB_NRZ=$(get-teb-by-snr $snr "NRZ")
+	TEB_NRZT=$(get-teb-by-snr $snr "NRZT")
+	echo "$snr,$TEB_RZ,$TEB_NRZ,$TEB_NRZT"
     done
 }
 git clone https://github.com/sapk-tb/SIT213.git tmp
