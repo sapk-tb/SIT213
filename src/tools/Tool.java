a7ea5a50715c42910aff8533c7621afaef1f4fb2
Antoine GIRARD <sapk@sapk.fr>
2015-10-09 17:44:34 +0200

    Split calcul TEB for more specifique testing

--- src/tools/Tool.java
+++ src/tools/Tool.java
@@ -25,25 +25,45 @@ public class Tool {
      return total / inf.nbElements();
      }
      */
+
+    /**
+     * Retourne le pourcentage de différence entre deux tableau de Boolean
+     *
+     * @param a Le premier tableau
+     * @param b le seconds tableau
+     * @return
+     */
+    public static float compare(Boolean[] a, Boolean[] b) {
+        int nbSymbole = a.length;
+        int errors = 0;
+        for (int i = 0; i < nbSymbole; i++) {
+            if (a[i] != b[i]) {
+                errors++;
+            }
+        }
+        return (float) errors / (float) nbSymbole;
+    }
+
     //*
     public static float getPuissance(Information<Float> inf) {
         Double total = 0.0;
         for (Float content : inf) {
             total += Math.pow(content, 2);
         }
-        return (float)(total / (float)inf.nbElements());
-    }
-    //*/
-    /*
-    public static float getPuissance(Information<Float> inf) {
-
-        Float total = inf.getStream().reduce(0f, Float::sum);
-//        double total = inf.getStream().mapToDouble(d->d).sum();
-                //.mapToDouble(Double::doubleValue).sum();
-
         return (float) (total / (float) inf.nbElements());
     }
     //*/
+    /*
+     public static float getPuissance(Information<Float> inf) {
+
+     Float total = inf.getStream().reduce(0f, Float::sum);
+     //        double total = inf.getStream().mapToDouble(d->d).sum();
+     //.mapToDouble(Double::doubleValue).sum();
+
+     return (float) (total / (float) inf.nbElements());
+     }
+     //*/
+
     public static float dBToLin(float db) {
         return (float) Math.pow(10, db / 10f);
     }
