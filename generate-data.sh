26d0e6dd1489894942988840ddca25b9506a314b
Antoine GIRARD <sapk@users.noreply.github.com>
2015-10-16 20:24:54 +0200

    Update generate-data.sh

--- generate-data.sh
+++ generate-data.sh
@@ -5,7 +5,7 @@ function get-teb-by-snr {
     NB_SYM=${3:-999999}
     NB_ECH=${4:-30}
     FORM=${2:-"NRZ"}
-    ARGS="-form $FORM -mess $NB_SYM -nbEch $NB_ECH -ampl -1 1 -stat-img "
+    ARGS="-form $FORM -mess $NB_SYM -nbEch $NB_ECH -ampl -1 1 "
     ./simulateur $ARGS -snr $1 | grep TEB | cut -d":" -f2 | tr -d " "
 }
 function generate-teb-by-snr {
@@ -15,8 +15,19 @@ function generate-teb-by-snr {
     do
         TEB_RZ=$(get-teb-by-snr $snr "RZ" $3 $4)
         TEB_NRZ=$(get-teb-by-snr $snr "NRZ" $3 $4)
-		TEB_NRZT=$(get-teb-by-snr $snr "NRZT" $3 $4)
-		echo "$snr,$TEB_RZ,$TEB_NRZ,$TEB_NRZT"
+	TEB_NRZT=$(get-teb-by-snr $snr "NRZT" $3 $4)
+	echo "$snr,$TEB_RZ,$TEB_NRZ,$TEB_NRZT"
+    done
+}
+function generate-oeil {
+    NB_SYM=${3:-9999}
+    NB_ECH=${4:-30}
+    ARGS="-mess $NB_SYM -nbEch $NB_ECH -ampl -1 1 -stat-img data/img 1024 "
+    for snr in $(seq $1 1 $2)
+    do
+    	xvfb-run ./simulateur $ARGS -snr $nr -form "RZ"
+    	xvfb-run ./simulateur $ARGS -snr $nr -form "NRZ"
+    	xvfb-run ./simulateur $ARGS -snr $nr -form "NRZT"
     done
 }
 rm data/img/*
@@ -29,8 +40,16 @@ for nbSym in 9 99 999 9999 99999 999999
 	for nbEch in 3 5 10 15 30 60
 		do
 		echo "generate-teb-by-snr -60 10 $nbSym $nbEch"
-    	time generate-teb-by-snr -60 10 $nbSym $nbEch > "../data/teb-by-snr-$nbSym-$nbEch.csv"
-    done
+    		time generate-teb-by-snr -60 10 $nbSym $nbEch > "../data/teb-by-snr-$nbSym-$nbEch.csv"
+    	done
+done
+for nbSym in 9 99 999 9999
+	do
+	for nbEch in 3 5 10 15 30 60
+		do
+		echo "generate-oeil -60 10 $nbSym $nbEch"
+    		time generate-oeil -60 10 $nbSym $nbEch
+    	done
 done
 cd ..
 rm -Rf tmp
