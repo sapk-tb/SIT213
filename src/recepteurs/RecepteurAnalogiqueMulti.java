cf6ae7eb981a6882cff0891b564dce56d8028da0
Antoine GIRARD <sapk@sapk.fr>
2015-10-17 17:15:06 +0200

    Very little opti

--- src/recepteurs/RecepteurAnalogiqueMulti.java
+++ src/recepteurs/RecepteurAnalogiqueMulti.java
@@ -109,11 +109,20 @@ public class RecepteurAnalogiqueMulti extends Recepteur<Float, Boolean> {
         		dtmax=dt[i];
         	}
         }
-        Information infoDecodee=new Information();
-        for(int i=0; i<(informationRecue.nbElements()-dtmax);i++){
+        
+        int nbEchTotal = informationRecue.nbElements()-dtmax;
+        int nbSymbole = nbEchTotal / nbEch;
+        
+        //TODO optimize
+        //TODO check if dt = 0 and possible conflict
+        Information infoDecodee=new Information(nbEchTotal);
+        for(int i=0; i<(nbEchTotal);i++){
         	infoDecodee.addAt(i, informationRecue.iemeElement(i));
         	for(int j=0;j<dt.length;j++){
-        		if((i-dt[j])>0){
+                        if(ar[j] == 0){
+                            continue; //l'amplitude de la reflection est de 0 -> on passe
+                        }
+        		if((i-dt[j])>0){ //TODO defini if not better >=
         			float valeur=(float)infoDecodee.iemeElement(i-dt[j])*ar[j];
         			infoDecodee.setIemeElement(i,(float)infoDecodee.iemeElement(i) - valeur);
         		}
@@ -122,8 +131,6 @@ public class RecepteurAnalogiqueMulti extends Recepteur<Float, Boolean> {
         //Fin debruitage du signal
         
         
-        int nbEchTotal = informationRecue.nbElements();
-        int nbSymbole = nbEchTotal / nbEch;
         Information<Boolean> informationAEmettre = new Information<Boolean>(nbSymbole);
         //Float allEch[] = new Float[nbEchTotal];
         float total[] = new float[nbSymbole];
