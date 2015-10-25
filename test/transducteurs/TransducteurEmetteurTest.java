de8e2ede796dd047ca5171aba35014ba45b661fb
CedricHerzog <CedricHerzog@users.noreply.github.com>
2015-10-25 17:32:22 +0100

    Changement Test

--- test/transducteurs/TransducteurEmetteurTest.java
+++ test/transducteurs/TransducteurEmetteurTest.java
@@ -26,8 +26,18 @@ public class TransducteurEmetteurTest {
         @Test
         public void testGenererInformation() throws Exception{
             Information<Boolean> informationRecue= new Information<Boolean>();
-            informationRecue={true, false};
-            Information<Boolean> informationEmise={true, false, true, false, true, false};
+            informationRecue.add(true);
+            informationRecue.add(false);
+            //informationRecue={true, false};
+            Boolean[] contenu={true, false, true, false, true, false};
+            Information<Boolean> informationEmise;
+            informationEmise.add(true);
+            informationEmise.add(false);
+            informationEmise.add(true);
+            informationEmise.add(false);
+            informationEmise.add(true);
+            informationEmise.add(false);
+            //Information<Boolean> informationEmise={true, false, true, false, true, false};
             Information <Boolean>  informationGeneree;
             int taille=2;
             int nbElements = informationRecue.nbElements();
@@ -35,7 +45,8 @@ public class TransducteurEmetteurTest {
             informationGeneree = new Information<>(nbElements * 3);
             TransducteurEmetteur instance= new TransducteurEmetteur();
             instance.emettre();
-            assertEquals(this.informationEmise, informationGeneree);
+            assertEquals(informationEmise, informationGeneree);
+            //assertEquals(this.informationEmise, informationGeneree);
         }
     
 }
