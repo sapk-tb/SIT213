a8816a9ae85a820f72a8ae2baec64b95b13755a4
Mélanie <Mlanie@pc-melanie>
2015-10-25 18:47:50 +0100

    Ajout test TransducteurRecepteurTest

---
+++ test/transducteurs/TransducteurRecepteurTest.java
@@ -0,0 +1,48 @@
+package transducteurs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import information.Information;
+
+/**
+*
+* @author Mélanie Corre
+*/
+
+public class TransducteurRecepteurTest {
+
+	@Test
+	public void testEmettre() {
+		
+	}
+
+	@Test
+	public void testTransducteurRecepteur() {
+		
+	}
+
+	@Test
+	public void testRecevoirInformationOfBoolean() {
+        Information<Boolean> informationEmise= new Information<Boolean>();
+        informationEmise.add(true);
+        informationEmise.add(false);
+        Information<Boolean> informationRecue=new Information<Boolean>();
+        informationRecue.add(true);
+        informationRecue.add(false);
+        informationRecue.add(true);
+        informationRecue.add(false);
+        informationRecue.add(true);
+        informationRecue.add(false);
+        Information <Boolean>  informationGeneree;
+        int nbElements = informationRecue.nbElements()/3;
+        Boolean tabBoolean[] = new Boolean[nbElements];
+        informationGeneree = new Information<>(tabBoolean);
+        TransducteurRecepteur instance = new TransducteurRecepteur();
+        System.out.println("Test methode : emettre");
+        instance.recevoir(informationRecue);
+        assertEquals(instance.informationEmise, instance.informationGeneree);
+	}
+
+}
